922e32767883cfb0217d3b87727c03817c08940f
Tim Heck <tih99924@bethel.edu>
2019-08-21 17:16:19 -0500

    get route set up with sinatra

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,4 @@
 require 'active_record'
-require_relative './models/pets'
 
 def db_configuration
     db_configuration_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
@@ -8,19 +7,17 @@ end
 
 ActiveRecord::Base.establish_connection(db_configuration["development"])
 
-print "Give me a pet name:"
-name = gets.chomp
+# print "Give me a pet name:"
+# name = gets.chomp
 
-print "Give me a pet breed:"
-breed = gets.chomp
+# print "Give me a pet breed:"
+# breed = gets.chomp
 
-print "Give me the pets color:"
-color = gets.chomp
+# print "Give me the pets color:"
+# color = gets.chomp
 
-print "Checked in?"
-checked_in = gets.chomp
+# print "Checked in?"
+# checked_in = gets.chomp
 
-name = Pets.new(name: name, breed: breed, color: color, checked_in: checked_in)
-name.save!
-
-puts "Number of pets in database: #{Pets.count}"
+# name = Pets.new(name: name, breed: breed, color: color, checked_in: checked_in)
+# name.save!
