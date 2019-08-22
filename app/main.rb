b1d0c81171328248c017c835e57894d869539814
Tim Heck <tih99924@bethel.edu>
2019-08-22 09:54:26 -0500

    cleaned up code

--- app/main.rb
+++ app/main.rb
@@ -6,18 +6,3 @@ def db_configuration
 end
 
 ActiveRecord::Base.establish_connection(db_configuration["development"])
-
-# print "Give me a pet name:"
-# name = gets.chomp
-
-# print "Give me a pet breed:"
-# breed = gets.chomp
-
-# print "Give me the pets color:"
-# color = gets.chomp
-
-# print "Checked in?"
-# checked_in = gets.chomp
-
-# name = Pets.new(name: name, breed: breed, color: color, checked_in: checked_in)
-# name.save!
